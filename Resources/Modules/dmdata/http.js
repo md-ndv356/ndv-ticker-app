dca19a5e975449c4b070ad90a09aa312f598790d
looksky495 (Mac) <tuyatuya_muhco@icloud.com>
2025-10-08 18:17:37 +0900

    Resources/Modules/dmdata/http.jsの雛形

---
+++ Resources/Modules/dmdata/http.js
@@ -0,0 +1,28 @@
+const oauth = require("./oauth");
+const http = require("../http_request");
+const axios = require("axios");
+
+/**
+ * 接続するAPIリスト
+ * Contract List v2 [contract.list] https://api.dmdata.jp/v2/contract
+ * Socket Start v2 [socket.start] https://api.dmdata.jp/v2/socket
+ * Socket List v2 [socket.list] https://api.dmdata.jp/v2/socket
+ * Socket Close v2 [socket.close] https://api.dmdata.jp/v2/socket/:id
+ * Telegram List v2 [telegram.list] https://api.dmdata.jp/v2/telegram
+ * Telegram Data v1 [telegram.data] https://data.api.dmdata.jp/v1/:id
+ * JmaFile List v2 [jmafile.list] https://api.dmdata.jp/v2/jmafile
+ * JmaFile Data v1 [jmafile.data] https://jmafiledata.api.dmdata.jp/v1/:id
+ * Archive List v2 [archive.list] https://api.dmdata.jp/v2/archive
+ * Archive Data v1 [archive.data] https://data.api.dmdata.jp/v1/archive/:id
+ * Parameter Earthquake station v2 [parameter.earthquake] https://api.dmdata.jp/v2/parameter/earthquake/station
+ * Parameter Realtime station v2 [parameter.realtime] https://api.dmdata.jp/v2/parameter/realtime/station
+ * Parameter Tsunami station v2 [parameter.tsunami] https://api.dmdata.jp/v2/parameter/tsunami/station
+ * GD Earthquake List [gd.earthquake] https://api.dmdata.jp/v2/gd/earthquake
+ * GD Earthquake Event [gd.earthquake] https://api.dmdata.jp/v2/gd/earthquake/:eventId
+ * GD Eew List [gd.eew] https://api.dmdata.jp/v2/gd/eew
+ * GD Eew Event [gd.eew] https://api.dmdata.jp/v2/gd/eew/:eventId
+ */
+
+module.exports = {
+
+};
